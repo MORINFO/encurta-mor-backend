73edadafa6935739a2a13746e3042ca2f7a1bc16
BrenoNovais <breno.novais@hotmail.com>
2022-09-23 17:13:53 -0300

    update 999

--- src/database.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export default prisma
-
-// use `prisma` in your application to read and write data in your DB
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import prisma from "./database";
+import 'dotenv/config'
 
 const app = express()
 
@@ -8,19 +8,6 @@ app.use(express.json())
 app.get("/home", async (req, res) => {
 
     return res.json({"message": "hello"})
-})
-
-app.post("/home", async (req, res) => {
-
-    const {email, link } = req.body
-    const encurta_link = await prisma.links.create({
-        data: {
-            email: email,
-            link_original: link,
-            link_encurtado: '12345'
-        }
-    })
-    return res.json(encurta_link)
 })
 
 app.listen(process.env.PORT, ()=> {console.log('servidor aberto!')})
