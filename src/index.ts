5b61d7082152e963e33ae846354fce93ae72bea7
BrenoNovais <breno.novais@hotmail.com>
2022-09-23 16:45:07 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import prisma from "./database";
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/", async (req, res) => {
+
+    const {email, link } = req.body
+    const encurta_link = await prisma.links.create({
+        data: {
+            email: email,
+            link_original: link,
+            link_encurtado: '12345'
+        }
+    })
+    return res.json(encurta_link)
+})
+
+app.listen(3456, ()=> {console.log('servidor aberto!')})
