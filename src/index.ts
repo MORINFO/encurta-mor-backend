e88b6dcfc1c95d637ffe3d926bbacc63330aa05b
BrenoNovais <breno.novais@hotmail.com>
2025-07-30 14:15:03 -0300

    fix: remover configuração CORS do arquivo index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,6 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-    origin: ["https://morinfo.com.br", "https://encurta.morinfo.com.br", "http://localhost:3000", "https://e.morinfo.com.br", "https://em.fly.dev", "https://encurta-backend.srv.morinfo.com.br"]
-}))
-
 app.get("/home", async (req, res) => {
 
     return res.json({ "message": "helloupdate" })
