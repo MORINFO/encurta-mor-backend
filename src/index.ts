3de21f87d2113e10087514265103bbf4c950b518
BrenoNovais <breno.novais@hotmail.com>
2025-07-30 16:47:02 -0300

    fix: atualizar mensagem de resposta da rota /home e garantir configuração CORS

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import 'dotenv/config';
 import express from "express";
 import Routes from "./Routes";
@@ -6,9 +7,10 @@ const app = express()
 
 app.use(express.json())
 
+app.use(cors({ origin: ["https://encurta.morinfo.com.br"] }))
 app.get("/home", async (req, res) => {
 
-    return res.json({ "message": "hello" })
+    return res.json({ "message": "helloupdate" })
 })
 
 app.use(Routes)
