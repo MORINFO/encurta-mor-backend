32603f6172b05b8765641d1f0de1c58940a01e53
BrenoNovais <breno.novais@hotmail.com>
2025-07-30 14:11:19 -0300

    fix: adicionar novo domínio à lista de origens permitidas no CORS

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ["https://morinfo.com.br", "https://encurta.morinfo.com.br", "http://localhost:3000", "https://e.morinfo.com.br", "https://em.fly.dev"]
+    origin: ["https://morinfo.com.br", "https://encurta.morinfo.com.br", "http://localhost:3000", "https://e.morinfo.com.br", "https://em.fly.dev", "https://encurta-backend.srv.morinfo.com.br"]
 }))
 
 app.get("/home", async (req, res) => {
